572d49daeeef219f81804861eebbfe996d701683
Debychina <rahwabirukgebre@gmail.com>
2023-08-17 12:19:28 -0700

    C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - A program that tests function if it's positive or negative
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positve_or_negative(i);
+
+	return (0);
+}
